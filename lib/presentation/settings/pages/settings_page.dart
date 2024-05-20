a33a48ffecdf3cf1897fa19477c0e34f742b8fc6
julienco03 <julian160903@gmail.com>
2024-05-20 11:11:00 +0200

    Create folder for profile and settings

---
+++ lib/presentation/settings/pages/settings_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:gymbuddies/presentation/common/widgets/app_bar.dart';
+import 'package:gymbuddies/presentation/common/widgets/bottom_navigation_bar.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+
+  @override
+  CalendarPageState createState() => CalendarPageState();
+}
+
+class CalendarPageState extends State<CalendarPage> {
+  final int _currentIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: MyAppBar(),
+      ),
+      body: null,
+      bottomNavigationBar: MyBottomNavigationBar(currentIndex: _currentIndex),
+    );
+  }
+}
