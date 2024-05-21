37b6d498543927c15059ad223000d2fc57758c4e
julienco03 <julian160903@gmail.com>
2024-05-21 12:25:21 +0200

    Implement contacts page

--- lib/presentation/contacts/pages/contacts_page.dart
+++ lib/presentation/contacts/pages/contacts_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gymbuddies/presentation/common/widgets/app_bar.dart';
 import 'package:gymbuddies/presentation/common/widgets/bottom_navigation_bar.dart';
+import 'package:gymbuddies/presentation/contacts/widgets/add_new_contact_widget.dart';
+import 'package:gymbuddies/presentation/contacts/widgets/contact_item.dart';
 
 class ContactsPage extends StatefulWidget {
   const ContactsPage({super.key});
@@ -11,6 +13,7 @@ class ContactsPage extends StatefulWidget {
 
 class ContactsPageState extends State<ContactsPage> {
   final int _currentIndex = 3;
+  final String _contactsListTitle = 'Buddies';
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +22,55 @@ class ContactsPageState extends State<ContactsPage> {
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: MyAppBar(),
       ),
-      body: null,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              _contactsListTitle,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Expanded(
+              flex: 1,
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: List.generate(
+                          10,
+                          (index) => ContactItem(
+                            contactName: 'Buddy ${index + 1}',
+                            trainingInformation:
+                                'Trainings: $index | last trained: 5 May',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            const AddNewContactWidget(),
+          ],
+        ),
+      ),
       bottomNavigationBar: MyBottomNavigationBar(currentIndex: _currentIndex),
     );
   }
