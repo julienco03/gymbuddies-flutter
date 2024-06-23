090d39b96bd571cec16bee35d55cd90125e99042
julienco03 <julian160903@gmail.com>
2024-06-23 09:42:26 +0200

    Declare go routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gymbuddies/presentation/training/pages/ongoing_training_page.dart';
-import 'package:gymbuddies/presentation/training/widgets/training_plan_detail_view.dart';
+import 'package:go_router/go_router.dart';
 
 import 'presentation/auth/pages/login_page.dart';
 import 'presentation/auth/pages/signup_page.dart';
@@ -10,49 +9,85 @@ import 'presentation/contacts/pages/contacts_page.dart';
 import 'presentation/home/pages/home_page.dart';
 import 'presentation/profile/pages/profile_page.dart';
 import 'presentation/settings/pages/settings_page.dart';
+import 'presentation/training/pages/ongoing_training_page.dart';
 import 'presentation/training/pages/training_plans_page.dart';
 import 'presentation/training/pages/start_training_page.dart';
+import 'presentation/training/widgets/training_plan_detail_view.dart';
+
+GoRouter defineRoutes() {
+  return GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/signup',
+        builder: (context, state) => const SignupPage(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => const ProfilePage(),
+      ),
+      GoRoute(
+        path: '/settings',
+        builder: (context, state) => const SettingsPage(),
+      ),
+      GoRoute(
+        path: '/calendar',
+        builder: (context, state) => const CalendarPage(),
+      ),
+      GoRoute(
+        path: '/contacts',
+        builder: (context, state) => const ContactsPage(),
+      ),
+      GoRoute(
+        path: '/training',
+        builder: (context, state) => const TrainingPage(),
+      ),
+      GoRoute(
+        path: '/training/start',
+        builder: (context, state) => const TrainingStartPage(),
+      ),
+      GoRoute(
+        path: '/training/ongoing-training',
+        builder: (context, state) => const OngoingTrainingPage(),
+      ),
+      GoRoute(
+        path: '/training/detail/:trainingPlanId',
+        builder: (context, state) {
+          final trainingPlanId =
+              int.parse(state.pathParameters['trainingPlanId']!);
+          return TrainingPlanDetailView(trainingPlanId: trainingPlanId);
+        },
+      ),
+    ],
+  );
+}
 
 void main() {
-  runApp(const MyApp());
+  final router = defineRoutes();
+  runApp(MyApp(router: router));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GoRouter router;
+  const MyApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
       debugShowCheckedModeBanner: false,
       title: 'Gymbuddies',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.dark,
-      initialRoute: '/login',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/signup': (context) => const SignupPage(),
-        '/login': (context) => const LoginPage(),
-        '/profile': (context) => const ProfilePage(),
-        '/settings': (context) => const SettingsPage(),
-        '/calendar': (context) => const CalendarPage(),
-        '/contacts': (context) => const ContactsPage(),
-        '/training': (context) => const TrainingPage(),
-        '/training/start': (context) => const TrainingStartPage(),
-        '/training/ongoing-training': (context) => const OngoingTrainingPage(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/training/detail') {
-          final args = settings.arguments as Map<String, dynamic>;
-          final int trainingPlanId = args['trainingPlanId'] as int;
-          return MaterialPageRoute(
-            builder: (context) {
-              return TrainingPlanDetailView(trainingPlanId: trainingPlanId);
-            },
-          );
-        }
-        return null;
-      },
     );
   }
 }
